90fc1bf51e2d8452ba5ff48fe2a4938c390b97ec
Chris Ware <chris.ware@ft.com>
2013-03-25 09:23:03 +0000

    Added Nodetime

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-require('nodefly').profile(
-    process.env.NODEFLY_APPLICATION_KEY,
-    [process.env.APPLICATION_NAME,'Heroku']
-);
+//require('nodefly').profile(
+//    process.env.NODEFLY_APPLICATION_KEY,
+//    [process.env.APPLICATION_NAME, 'Heroku']
+//);
+
+if(process.env.NODETIME_ACCOUNT_KEY) {
+    require('nodetime').profile({
+        accountKey: process.env.NODETIME_ACCOUNT_KEY,
+        appName: 'Wrapper client' // optional
+    });
+}
 
 var express = require("express"),
     logger = require("ft-node-modules/logger").init({
@@ -12,15 +19,59 @@ var express = require("express"),
         }
     }),
     wrapper = require("ft-node-modules/wrapper"),
-    model = require("./model");
+    model = require("./model"),
+    dot = require("express-dot");
+
+//dot.setGlobals({
+//    // global configuration
+//    // default is false, set true in production enviroment to cache partials
+//    partialCache: false,
+//    layout: false
+//});
 
 var app = express();
 
+app.configure(function () {
+    // set views folder
+    app.set('views', __dirname + '/views');
 
-app.get('/', function (request, response) {
-    response.send("Wrapper client running");
+    // doT engine
+    app.set('view engine', 'dot');
+    app.engine('dot', dot.__express);
+
+    app.use(express.logger());
+    app.use(express.compress());
+    app.use(express.methodOverride());
+
+    app.use(express.static('public'))
+
 });
 
+/**
+ * Validate parameters
+ * http://expressjs.com/api.html#app.param
+ */
+app.param(function (name, fn) {
+    if (fn instanceof RegExp) {
+        return function (req, res, next, val) {
+            var captures;
+            if (captures = fn.exec(String(val))) {
+                req.params[name] = captures;
+                next();
+            } else {
+                next('route');
+            }
+        }
+    }
+});
+
+app.param("uuid", /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}(\.html)?$/);
+
+app.get("/cms/s/:uuid", function (req, res) {
+    res.send("UUID: " + req.params.uuid);
+});
+
+//app.param("id", /^\s|\S?$/);
 app.get('/wrapper/:id', function (request, response) {
     var wrapperId = request.params.id,
         result;
@@ -32,6 +83,10 @@ app.get('/wrapper/:id', function (request, response) {
     });
 });
 
+app.get('/', function (request, response) {
+    //response.send("Wrapper client running");
+    response.render("index", {});
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
