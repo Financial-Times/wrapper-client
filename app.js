f7afce177b9df340f22c883baf56dceb05d2d18a
Chris Ware <chris.ware@ft.com>
2013-03-25 09:46:44 +0000

    Added Cluster around app.js.  Apparently Heroku has 4 cores per Dyno.

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-//require('nodefly').profile(
-//    process.env.NODEFLY_APPLICATION_KEY,
-//    [process.env.APPLICATION_NAME, 'Heroku']
-//);
+require('nodefly').profile(
+    process.env.NODEFLY_APPLICATION_KEY,
+    [process.env.APPLICATION_NAME, 'Heroku']
+);
 
-if(process.env.NODETIME_ACCOUNT_KEY) {
+if (process.env.NODETIME_ACCOUNT_KEY) {
     require('nodetime').profile({
         accountKey: process.env.NODETIME_ACCOUNT_KEY,
         appName: 'Wrapper client' // optional
     });
 }
 
-var express = require("express"),
-    logger = require("ft-node-modules/logger").init({
-        loggly: {
-            logglyKey: "28e914ff-2027-43ca-b1d5-d9624dbe8e49",
-            logglyDomain: "financialtimes",
-            logLevel: "info"
-        }
-    }),
-    wrapper = require("ft-node-modules/wrapper"),
-    model = require("./model"),
-    dot = require("express-dot");
+
+var cluster = require("cluster"),
+    numCPUs = require("os").cpus().length;
+
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; ++i)
+        cluster.fork();
+} else {
+    var express = require("express"),
+        logger = require("ft-node-modules/logger").init({
+            loggly: {
+                logglyKey: "28e914ff-2027-43ca-b1d5-d9624dbe8e49",
+                logglyDomain: "financialtimes",
+                logLevel: "info"
+            }
+        }),
+        wrapper = require("ft-node-modules/wrapper"),
+        model = require("./model"),
+        dot = require("express-dot");
 
 //dot.setGlobals({
 //    // global configuration
@@ -29,66 +37,67 @@ var express = require("express"),
 //    layout: false
 //});
 
-var app = express();
+    var app = express();
 
-app.configure(function () {
-    // set views folder
-    app.set('views', __dirname + '/views');
+    app.configure(function () {
+        // set views folder
+        app.set('views', __dirname + '/views');
 
-    // doT engine
-    app.set('view engine', 'dot');
-    app.engine('dot', dot.__express);
+        // doT engine
+        app.set('view engine', 'dot');
+        app.engine('dot', dot.__express);
 
-    app.use(express.logger());
-    app.use(express.compress());
-    app.use(express.methodOverride());
+        app.use(express.logger());
+        app.use(express.compress());
+        app.use(express.methodOverride());
 
-    app.use(express.static('public'))
+        app.use(express.static('public'))
 
-});
+    });
 
-/**
- * Validate parameters
- * http://expressjs.com/api.html#app.param
- */
-app.param(function (name, fn) {
-    if (fn instanceof RegExp) {
-        return function (req, res, next, val) {
-            var captures;
-            if (captures = fn.exec(String(val))) {
-                req.params[name] = captures;
-                next();
-            } else {
-                next('route');
+    /**
+     * Validate parameters
+     * http://expressjs.com/api.html#app.param
+     */
+    app.param(function (name, fn) {
+        if (fn instanceof RegExp) {
+            return function (req, res, next, val) {
+                var captures;
+                if (captures = fn.exec(String(val))) {
+                    req.params[name] = captures;
+                    next();
+                } else {
+                    next('route');
+                }
             }
         }
-    }
-});
+    });
 
-app.param("uuid", /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}(\.html)?$/);
+    app.param("uuid", /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}(\.html)?$/);
 
-app.get("/cms/s/:uuid", function (req, res) {
-    res.send("UUID: " + req.params.uuid);
-});
+    app.get("/cms/s/:uuid", function (req, res) {
+        res.send("UUID: " + req.params.uuid);
+    });
 
 //app.param("id", /^\s|\S?$/);
-app.get('/wrapper/:id', function (request, response) {
-    var wrapperId = request.params.id,
-        result;
+    app.get('/wrapper/:id', function (request, response) {
+        var wrapperId = request.params.id,
+            result;
 
-    wrapper.fetch(wrapperId, function (err, html) {
-        console.info("Fetched " + request.params.id);
-        result = wrapper.process(html, model.populate());
-        response.send(result);
+        wrapper.fetch(wrapperId, function (err, html) {
+            console.info("Fetched " + request.params.id);
+            result = wrapper.process(html, model.populate());
+            response.send(result);
+        });
     });
-});
 
-app.get('/', function (request, response) {
-    //response.send("Wrapper client running");
-    response.render("index", {});
-});
+    app.get('/', function (request, response) {
+        //response.send("Wrapper client running");
+        response.render("index", {});
+    });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function () {
-    console.info("Listening on " + port);
-});
+    var port = process.env.PORT || 5000;
+    app.listen(port, function () {
+        console.info("Listening on " + port);
+    });
+}
